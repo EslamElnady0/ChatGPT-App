a65ba4c322091b318b289d9f58e0e4e53020ec41
EslamElnady0 <eslamelnady254@gmail.com>
2023-04-11 12:20:54 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(ChatGPT());
+}
+
+class ChatGPT extends StatelessWidget {
+  const ChatGPT({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
