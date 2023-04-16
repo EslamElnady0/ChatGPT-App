762c2ec81a65ae3aeae33801ca6c850be483b40d
EslamElnady0 <eslamelnady254@gmail.com>
2023-04-16 18:52:43 +0200

    new api key

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,2 +1,2 @@
 const baseUrl = "https://api.openai.com/v1";
-const apiKey = "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";
+const apiKey = "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";
