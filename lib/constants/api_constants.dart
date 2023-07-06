deac6206991ee77142c89f4905e6c2523c0f3493
EslamElnady0 <eslamelnady254@gmail.com>
2023-07-06 10:47:27 +0300

    changed api key

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,2 +1,2 @@
 const baseUrl = "https://api.openai.com/v1";
-const apiKey = "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";
+const apiKey = "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";
